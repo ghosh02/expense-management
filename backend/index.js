b65895f84a6c4b540ad961be62c3791fe861d89c
Sudip Ghosh <sudip02ghosh@gmail.com>
2025-05-09 02:27:45 +0530

    cors2

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://expense-management-q5fj.onrender.com",
+    origin: true,
     credentials: true,
   })
 );
